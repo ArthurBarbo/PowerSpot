5030097fd7286c0226809042b61819b06befea8d
Arthur BARBOSA <arthurbarbo177@gmail.com>
2025-11-10 22:14:25 +0000

    finalização da fase 1 de estilo do header e do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,16 @@ export default function Footer() {
   return (
     <footer className="footer__container">
       <p className="footer__text">©2025 PowerSpot -  Arthur Oliveira. All rights reserved.</p>
+      <div className="footer__links-container">
+        <a href="#" className="footer__link">
+          <img src= "../../public/linkedin.svg" alt="LinkedIn" className="footer__icon" />
+          Linkedin
+        </a>
+        <a href="#" className="footer__link">
+          <img src= "../../public/github.svg" alt="Github" className="footer__icon" />
+          Github
+        </a>
+      </div>
     </footer>
   );
 }
